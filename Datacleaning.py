eb4dfa846f0b1511b2d1c56481bb4f61e689a8b8
baibhab007 <41581003+baibhab007@users.noreply.github.com>
2019-08-21 22:17:59 +0200

    Update Datacleaning.py

--- Datacleaning.py
+++ Datacleaning.py
@@ -23,3 +23,12 @@ Drop the rows having null values in any of the columns, and assign the result to
 Display the dataframe df_A2.
 ####
 
+import numpy as np
+import pandas as pd
+heights_A = pd.Series([176.2, 158.4, 167.6, 156.2, 161.4],index=['s1','s2','s3','s4','s5'])
+weights_A = pd.Series([85.1, 90.2, 76.8, 80.4, 78.9],index=['s1','s2','s3','s4','s5'])
+df_A=pd.DataFrame({'Student_height': heights_A, 'Student_weight': weights_A})
+df_A.iloc[2]=np.nan
+df_A['s5']=np.nan
+df_A2=df_A.dropna(how='any')
+print(df_A2)
