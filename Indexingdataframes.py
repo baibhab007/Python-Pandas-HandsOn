5e53eeb8033c7aa367ad57afdedb5bbd9b78806a
baibhab007 <41581003+baibhab007@users.noreply.github.com>
2019-09-03 15:19:39 +0200

    Update Indexingdataframes.py

--- Indexingdataframes.py
+++ Indexingdataframes.py
@@ -23,3 +23,26 @@ Capture the result in the variable 'dates_to_be_searched' and print it.
 datelist = ['14-Sep-2017', '9-Sep-2017']
 dates_to_be_searched = pd.to_datetime(datelist)
 print(dates_to_be_searched)
+
+####
+Task 3
+Check if the elements of 'dates_to_be_searched' are present in DateTimeIndex, 'dates', created above.
+Note: Use the isin method associated with DatetimeIndex objects.
+
+Print the output.
+####
+
+print((dates_to_be_searched).isin(dates))
+
+####
+Task 4
+Create a multi index named mi_index of two levels, represented in the following array arraylist: arraylist = [['classA']*5 + ['classB']*5, ['s1', 's2', 's3','s4', 's5']*2] .
+Note: Use MultiIndex of pandas.
+
+Print levels of mi_index.
+####
+
+arraylist = [['classA']*5 + ['classB']*5, ['s1', 's2', 's3','s4', 's5']*2])
+mi_index = pd.MultiIndex.from_arrays(arraylist)
+print(mi_index)
+
